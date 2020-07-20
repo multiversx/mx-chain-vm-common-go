d37ba1c475a00aeb84e1f4d90e18083083553f34
Cristi Corcoveanu <cristi.corcoveanu@gmail.com>
2020-07-20 13:08:23 +0300

    Add payable metadata field

--- codeMetadata_test.go
+++ codeMetadata_test.go
@@ -7,11 +7,15 @@ import (
 )
 
 func TestCodeMetadata_FromBytes(t *testing.T) {
-	require.True(t, CodeMetadataFromBytes([]byte{1}).Upgradeable)
-	require.False(t, CodeMetadataFromBytes([]byte{0}).Upgradeable)
+	require.True(t, CodeMetadataFromBytes([]byte{1, 0}).Upgradeable)
+	require.True(t, CodeMetadataFromBytes([]byte{0, 2}).Payable)
+	require.False(t, CodeMetadataFromBytes([]byte{0, 0}).Upgradeable)
+	require.False(t, CodeMetadataFromBytes([]byte{0, 0}).Payable)
 }
 
 func TestCodeMetadata_ToBytes(t *testing.T) {
 	require.Equal(t, byte(0), (&CodeMetadata{}).ToBytes()[0])
+	require.Equal(t, byte(0), (&CodeMetadata{}).ToBytes()[1])
 	require.Equal(t, byte(1), (&CodeMetadata{Upgradeable: true}).ToBytes()[0])
+	require.Equal(t, byte(2), (&CodeMetadata{Payable: true}).ToBytes()[1])
 }
