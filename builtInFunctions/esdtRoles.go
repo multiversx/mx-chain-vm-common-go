3ccca07decfc3bce0c007876dbce4eaeea6129ff
Robert Sasu <sasu.robert@gmail.com>
2021-09-14 10:38:58 +0300

    new logic for isSCCallAfter

--- builtInFunctions/esdtRoles.go
+++ builtInFunctions/esdtRoles.go
@@ -2,6 +2,7 @@ package builtInFunctions
 
 import (
 	"bytes"
+	"math"
 
 	"github.com/ElrondNetwork/elrond-go-core/core"
 	"github.com/ElrondNetwork/elrond-go-core/core/check"
@@ -67,6 +68,16 @@ func (e *esdtRoles) ProcessBuiltinFunction(
 		deleteRoles(roles, vmInput.Arguments[1:])
 	}
 
+	for _, arg := range vmInput.Arguments[1:] {
+		if bytes.Equal(arg, []byte(core.ESDTRoleNFTCreateMultiShard)) {
+			err = saveLatestNonce(acntDst, vmInput.Arguments[0], computeStartNonce(vmInput.RecipientAddr))
+			if err != nil {
+				return nil, err
+			}
+			break
+		}
+	}
+
 	err = saveRolesToAccount(acntDst, esdtTokenRoleKey, roles, e.marshalizer)
 	if err != nil {
 		return nil, err
@@ -74,6 +85,15 @@ func (e *esdtRoles) ProcessBuiltinFunction(
 
 	vmOutput := &vmcommon.VMOutput{ReturnCode: vmcommon.Ok}
 	return vmOutput, nil
+}
+
+// Nonces on multi shard NFT create ar from (LastByte * MaxUint64 / 256), this is in order to differentiate them
+// even like this, if one contract makes 1000 NFT create on each block, it would need 14 million years to occupy the whole space
+// 2 ^ 64 / 256 / 1000 / 14400 / 365 ~= 14 million
+func computeStartNonce(destAddress []byte) uint64 {
+	lastByteOfAddress := uint64(destAddress[len(destAddress)-1])
+	startNonce := (math.MaxUint64 / 256) * lastByteOfAddress
+	return startNonce
 }
 
 func deleteRoles(roles *esdt.ESDTRoles, deleteRoles [][]byte) {
@@ -134,14 +154,21 @@ func (e *esdtRoles) CheckAllowedToExecute(account vmcommon.UserAccountHandler, t
 	if isNew {
 		return ErrActionNotAllowed
 	}
+	if !searchForRole(roles, action) {
+		return ErrActionNotAllowed
+	}
 
+	return nil
+}
+
+func searchForRole(roles *esdt.ESDTRoles, action []byte) bool {
 	for _, role := range roles.Roles {
 		if bytes.Equal(role, action) {
-			return nil
+			return true
 		}
 	}
 
-	return ErrActionNotAllowed
+	return false
 }
 
 // IsInterfaceNil returns true if underlying object in nil
