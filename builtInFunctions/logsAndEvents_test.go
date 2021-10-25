fac68e73d9c863229ee6121bda0e7d77dc73db43
miiu96 <mihai9625@gmail.com>
2021-10-25 13:55:18 +0300

    log esdt wipe

--- builtInFunctions/logsAndEvents_test.go
+++ builtInFunctions/logsAndEvents_test.go
@@ -1,6 +1,7 @@
 package builtInFunctions
 
 import (
+	"encoding/hex"
 	"math/big"
 	"testing"
 
@@ -20,4 +21,22 @@ func TestNewEntryForNFT(t *testing.T) {
 		Topics:     [][]byte{[]byte("my-token"), big.NewInt(0).SetUint64(5).Bytes(), big.NewInt(1).Bytes(), []byte("receiver")},
 		Data:       nil,
 	}, vmOutput.Logs[0])
+}
+
+func TestExtractTokenIdentifierAndNonceESDTWipe(t *testing.T) {
+	t.Parallel()
+
+	hexArg := "534b4537592d37336262636404"
+	args, _ := hex.DecodeString(hexArg)
+
+	identifier, nonce := extractTokenIdentifierAndNonceESDTWipe(args)
+	require.Equal(t, uint64(4), nonce)
+	require.Equal(t, []byte("SKE7Y-73bbcd"), identifier)
+
+	hexArg = "5745474c442d376662623930"
+	args, _ = hex.DecodeString(hexArg)
+
+	identifier, nonce = extractTokenIdentifierAndNonceESDTWipe(args)
+	require.Equal(t, uint64(0), nonce)
+	require.Equal(t, []byte("WEGLD-7fbb90"), identifier)
 }
