16df4b52d45aacb5838ad06866776a6f02bcbee2
Alin Cruceat <alyn_509@yahoo.com>
2021-11-18 17:20:46 +0200

    esdt local roles tests fix

--- esdtLocalRoles_test.go
+++ esdtLocalRoles_test.go
@@ -6,12 +6,12 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func EsdtLocalRoles_FromBytes(t *testing.T) {
+func TestEsdtLocalRoles_FromBytes(t *testing.T) {
 	require.True(t, EsdtLocalRolesFromBytes([]byte{0, 0, 0, 0, 0, 0, 0, 1}).Mint)
 	require.True(t, EsdtLocalRolesFromBytes([]byte{0, 0, 0, 0, 0, 0, 0, 2}).Burn)
 	require.True(t, EsdtLocalRolesFromBytes([]byte{0, 0, 0, 0, 0, 0, 0, 4}).NFTCreate)
-	require.True(t, EsdtLocalRolesFromBytes([]byte{0, 0, 0, 0, 0, 0, 1, 0}).NFTAddQuantity)
-	require.True(t, EsdtLocalRolesFromBytes([]byte{0, 0, 0, 0, 0, 0, 2, 0}).NFTBurn)
+	require.True(t, EsdtLocalRolesFromBytes([]byte{0, 0, 0, 0, 0, 0, 0, 8}).NFTAddQuantity)
+	require.True(t, EsdtLocalRolesFromBytes([]byte{0, 0, 0, 0, 0, 0, 0, 16}).NFTBurn)
 	require.False(t, EsdtLocalRolesFromBytes([]byte{0, 0, 0, 0, 0, 1, 1, 0}).Mint)
 	require.False(t, EsdtLocalRolesFromBytes([]byte{0, 0, 0, 0, 0, 1, 0, 0}).Burn)
 	require.False(t, EsdtLocalRolesFromBytes([]byte{0, 0, 0, 0, 0, 1, 0, 0}).NFTCreate)
@@ -19,10 +19,10 @@ func EsdtLocalRoles_FromBytes(t *testing.T) {
 	require.False(t, EsdtLocalRolesFromBytes([]byte{0, 0, 0, 0, 0, 1, 0, 0}).NFTBurn)
 }
 
-func EsdtLocalRoles_ToBytes(t *testing.T) {
-	require.Equal(t, byte(0), (&CodeMetadata{}).ToBytes()[0])
-	require.Equal(t, byte(0), (&CodeMetadata{}).ToBytes()[1])
-	require.Equal(t, byte(1), (&CodeMetadata{Upgradeable: true}).ToBytes()[0])
-	require.Equal(t, byte(2), (&CodeMetadata{Payable: true}).ToBytes()[1])
-	require.Equal(t, byte(4), (&CodeMetadata{Readable: true}).ToBytes()[0])
+func TestEsdtLocalRoles_ToBytes(t *testing.T) {
+	require.Equal(t, byte(0), (&EsdtLocalRoles{}).ToBytes()[0])
+	require.Equal(t, byte(0), (&EsdtLocalRoles{}).ToBytes()[1])
+	require.Equal(t, byte(1), (&EsdtLocalRoles{Mint: true}).ToBytes()[7])
+	require.Equal(t, byte(2), (&EsdtLocalRoles{Burn: true}).ToBytes()[7])
+	require.Equal(t, byte(4), (&EsdtLocalRoles{NFTCreate: true}).ToBytes()[7])
 }
