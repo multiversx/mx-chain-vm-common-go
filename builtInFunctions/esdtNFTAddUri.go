59cef9c67dfa8b9143ab571f35feea7c217fb369
miiu <mihai9625@gmail.com>
2021-12-29 11:24:02 +0200

    update logs

--- builtInFunctions/esdtNFTAddUri.go
+++ builtInFunctions/esdtNFTAddUri.go
@@ -77,11 +77,11 @@ func (e *esdtNFTAddUri) SetNewGasConfig(gasCost *vmcommon.GasCost) {
 	e.mutExecution.Unlock()
 }
 
-// ProcessBuiltinFunction resolves ESDT NFT add quantity function call
+// ProcessBuiltinFunction resolves ESDT NFT add uris function call
 // Requires 3 arguments:
 // arg0 - token identifier
 // arg1 - nonce
-// arg2 - attributes to add
+// arg[2:] - uris to add
 func (e *esdtNFTAddUri) ProcessBuiltinFunction(
 	acntSnd, _ vmcommon.UserAccountHandler,
 	vmInput *vmcommon.ContractCallInput,
@@ -129,7 +129,7 @@ func (e *esdtNFTAddUri) ProcessBuiltinFunction(
 		GasRemaining: vmInput.GasProvided - e.funcGasCost - gasCostForStore,
 	}
 
-	addESDTEntryInVMOutput(vmOutput, []byte(core.BuiltInFunctionESDTNFTAddURI), vmInput.Arguments[0], nonce, big.NewInt(0), vmInput.CallerAddr)
+	addESDTEntryInVMOutput(vmOutput, []byte(core.BuiltInFunctionESDTNFTAddURI), vmInput.Arguments[0], nonce, big.NewInt(0), vmInput.Arguments[2:]...)
 
 	return vmOutput, nil
 }
