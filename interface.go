9e43f6ab89564a9869ad7eb972c64265ede148dc
Camil Băncioiu <ioan.camil.bancioiu@gmail.com>
2022-11-28 19:04:49 +0200

    Rename method

--- interface.go
+++ interface.go
@@ -360,7 +360,7 @@ type EnableEpochsHandler interface {
 	IsTransferToMetaFlagEnabled() bool
 	IsESDTNFTImprovementV1FlagEnabled() bool
 	IsFixOldTokenLiquidityEnabled() bool
-	IsRuntimeStoreLimitEnabled() bool
+	IsRuntimeMemStoreLimitEnabled() bool
 
 	MultiESDTTransferAsyncCallBackEnableEpoch() uint32
 	FixOOGReturnCodeEnableEpoch() uint32
