73cf927d1b8ab847af9a904bf847275dedfe0f5f
miiu <mihai9625@gmail.com>
2024-06-14 13:33:53 +0300

    log event nft recreate

--- builtInFunctions/creator.go
+++ builtInFunctions/creator.go
@@ -560,7 +560,7 @@ func (b *builtInFuncCreator) CreateBuiltInFunctionContainer() error {
 		return err
 	}
 
-	newFunc, err = NewESDTMetaDataRecreateFunc(b.gasConfig.BuiltInCost.ESDTNFTRecreate, b.gasConfig.BaseOperationCost, b.accounts, globalSettingsFunc, b.esdtStorageHandler, setRoleFunc, b.enableEpochsHandler)
+	newFunc, err = NewESDTMetaDataRecreateFunc(b.gasConfig.BuiltInCost.ESDTNFTRecreate, b.gasConfig.BaseOperationCost, b.accounts, globalSettingsFunc, b.esdtStorageHandler, setRoleFunc, b.enableEpochsHandler, b.marshaller)
 	if err != nil {
 		return err
 	}
